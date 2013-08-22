76c68cdecfe448fb89e7505c9d80b1ec4a89d237
Kevin Whinnery <kevin.whinnery@gmail.com>
2013-08-22 15:39:36 -0500

    need webhook for php starter

---
+++ www/hello.php
@@ -0,0 +1,5 @@
+<?php header('Content-Type:text/xml'); ?>
+<Response>
+    <Say>Hello there! You have successfully configured a web hook.</Say>
+    <Say voice="woman">Good luck on your Twilio quest!</Say>
+</Response>
